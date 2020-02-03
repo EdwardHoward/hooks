e117f163d9dfd2e41c0b592439adbf8f3ebf193c
EdwardHoward <EdwardHoward@users.noreply.github.com>
2020-02-02 23:14:43 -0700

    Create useDebounce.ts

---
+++ useDebounce.ts
@@ -0,0 +1,17 @@
+function useDebounce(value, delay){
+   const [debouncedValue, setDebouncedValue] = useState(value);
+
+   useEffect(() => {
+      const timeout = setTimeout(() => {
+         setDebouncedValue(value);
+      }, delay);
+
+      return () => {
+         clearTimeout(timeout);
+      }
+   }, [value]);
+
+   return debouncedValue;
+}
+
+export default useDebounce;
